3bd60bd6b15272f5219d138236926c229d23262e
Moonshadow <last_enemy@naver.com>
2024-03-25 15:01:17 +0900

    Defining Routes

---
+++ app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Avatar from "./components/Avatar";
+
+const AboutUs = () => {
+  return (
+    <h1>About us page <Avatar/></h1>
+  )
+};
+
+export default AboutUs;
