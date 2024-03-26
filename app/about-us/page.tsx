97f57bfb029999c96259257378080b6256313a9b
Moonshadow <last_enemy@naver.com>
2024-03-26 14:21:56 +0900

    Layouts

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navigation from "../../componments/navigation";
 
 const AboutUs = () => {
   return (
     <div>
-      <Navigation/>
       <h1>About us page</h1>
     </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../componments/navigation";
-
 export default function Hello(){
   return (
     <div>
-      <Navigation/>
       <h1>Hello</h1>
     </div>
   )
