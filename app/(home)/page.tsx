11133b5af6be439a77d28875b8a671b73d7a55f9
ictus <last_enemy@naver.com>
2024-03-28 12:03:39 +0900

    Fetching on Sever Side and make Loading Component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,20 @@
-"use client"
-import {useEffect, useState} from "react";
+export const metadata = {
+  title: 'Home',
+}
 
-export default function Hello(){
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState();
-  const getMovies = async()=>{
-    const response=await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-    const json = await response.json()
-    setMovies(json)
-    setIsLoading(false)
-  }
-  useEffect(
-    ()=>{
-      getMovies()
-    },[]
-  )
+const API_URL="https://nomad-movies.nomadcoders.workers.dev/movies"
+
+async function getMovies(){
+  await new Promise(resolve => setTimeout(resolve,3000))
+  const res = await fetch(API_URL)
+  const json = await res.json()
+  return json
+}
+export default async function HomePage(){
+  const movies =await getMovies()
   return (
     <div>
-      {isLoading?'Loading...':JSON.stringify(movies)}
+      {JSON.stringify(movies)}
     </div>
   )
 }
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <h2>Loading...😮</h2>
+  )
+};
+
+export default Loading;
