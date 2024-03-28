70f7b8c406ab6f9a63e11200573bdaf81dc98365
ictus <last_enemy@naver.com>
2024-03-28 14:49:43 +0900

    Parallel Requests

---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieLoading = () => {
+  return (
+    <h2>Loading...😮</h2>
+  )
+};
+
+export default MovieLoading;
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import {API_URL} from "../../../(home)/page";
 
 export const metadata = {
   title: 'Movie'
 }
-const MovieDetail = ({params: {id}}: { params: { id: string } }) => {
+
+async function getMovie(id: string) {
+  return await (await fetch(`${API_URL}/${id}`)).json()
+}
+
+async function getVideos(id: string) {
+  return await (await fetch(`${API_URL}/${id}/videos`)).json()
+}
+
+const MovieDetail = async ({params: {id}}: { params: { id: string } }) => {
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)])
   return (
-    <h1>Movie {id}</h1>
+    <h1>{movie.title}</h1>
   )
 };
 
 export default MovieDetail;
+
