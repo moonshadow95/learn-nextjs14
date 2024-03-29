0efa8d930e121e98849592b54beb39268ccb2172
Moonshadow <last_enemy@naver.com>
2024-03-29 16:00:24 +0900

    Movie trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import {API_URL} from "../app/(home)/page";
+import style from "../styles/movie-info.module.css"
 
 async function getMovie(id: string) {
   return await (await fetch(`${API_URL}/${id}`)).json()
@@ -6,5 +7,13 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({id}: { id: string }) {
   const movie = await getMovie(id)
-  return <h6>{JSON.stringify(movie)}</h6>
+  return <div className={style.container}>
+    <img className={style.poster} src={movie.poster_path} alt={movie.title}/>
+    <div>
+      <h1>{movie.title}</h1>
+      <h3>⭐{movie.vote_average.toFixed(1)}</h3>
+      <p>{movie.overview}</p>
+      <a href={movie.hompage} target='_blank'>Home page &rarr;</a>
+    </div>
+  </div>
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import {API_URL} from "../app/(home)/page";
+import style from "../styles/movie-videos.module.css"
 
 async function getVideos(id: string) {
   return await (await fetch(`${API_URL}/${id}/videos`)).json()
@@ -6,5 +7,14 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({id}: { id: string }) {
   const videos = await getVideos(id)
-  return <h6>{JSON.stringify(videos)}</h6>
+  return <div className={style.container}>
+    {videos.map(video =>
+      <iframe
+        key={video.id} src={`https://youtube.com/embed/${video.key}`} title={video.name}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    )}
+  </div>
+
 }
